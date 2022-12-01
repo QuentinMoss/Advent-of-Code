ae0bb0b23e800d69e55e48fa051799aee74e3d78
Quentin Moss <quentin.moss@protonmail.com>
2022-12-01 11:10:13 -0800

    remove bins

--- 2022/rust/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/quentinmoss/github/Advent-of-Code/2022/rust/day1/target/debug/day1: /Users/quentinmoss/github/Advent-of-Code/2022/rust/day1/src/../../input.txt /Users/quentinmoss/github/Advent-of-Code/2022/rust/day1/src/main.rs
--- 2022/rust/day1/target/debug/deps/day1-55c746a024ad54ac.d
+++
@@ -1,6 +0,0 @@
-/Users/quentinmoss/github/Advent-of-Code/2022/rust/day1/target/debug/deps/day1-55c746a024ad54ac: src/main.rs src/../../input.txt
-
-/Users/quentinmoss/github/Advent-of-Code/2022/rust/day1/target/debug/deps/day1-55c746a024ad54ac.d: src/main.rs src/../../input.txt
-
-src/main.rs:
-src/../../input.txt:
